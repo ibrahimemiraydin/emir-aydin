314351f056522cd77ece62c6d62f6a22f306f48f
Emir Aydın <emiraydin1625@hotmail.com>
2024-12-11 23:48:32 +0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/<ibrahimemiraydin.github.io>/', // Repository adınızı buraya yazın
 })
