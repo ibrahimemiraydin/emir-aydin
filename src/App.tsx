b41bd66ca080b680b1d1f538b5c8c8b0253057da
Emir Aydın <emiraydin1625@hotmail.com>
2024-12-11 20:53:26 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Navbar";
